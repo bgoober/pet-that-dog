04945350792723bbeb0acd353cd2bd949c9816e4
Ben <bgoober2@protonmail.com>
2024-11-05 22:01:33 -0500

    add lofigirl spotify iframe to app

--- app/web/src/app/app-layout.tsx
+++ app/web/src/app/app-layout.tsx
@@ -1,11 +1,17 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import Dapp from './dapp'; // Import the Dapp component
 
 import { Link } from 'react-router-dom';
 import React from 'react';
 
 export function AppLayout({ children }: { children: ReactNode }) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div
       style={{
@@ -18,6 +24,31 @@ export function AppLayout({ children }: { children: ReactNode }) {
     >
       <div style={{ position: 'absolute', top: '5px', right: '5px' }}>
         <WalletMultiButton />
+      </div>
+      <div style={{ position: 'absolute', top: '5px', left: '5px' }}>
+        <button
+          onClick={toggleCollapse}
+          style={{
+            marginBottom: '5px',
+            background: '#512da8',
+            color: '#fffcee',
+            border: 'none',
+            borderRadius: '5px',
+            fontSize: '15px',
+          }}
+        >
+          {isCollapsed ? 'Expand Spotify' : 'Collapse Spotify'}
+        </button>
+        <iframe
+          className={isCollapsed ? 'collapsed' : ''}
+          style={{ borderRadius: '15px', transition: 'opacity 0.5s' }}
+          src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator&theme=1"
+          width="100%"
+          height="152"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
       </div>
       <div
         style={{
@@ -35,13 +66,18 @@ export function AppLayout({ children }: { children: ReactNode }) {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            // transform: 'scale(2)',
             overflow: 'hidden',
           }}
         >
           <Dapp />
         </div>
       </div>
+      <style>{`
+        .collapsed {
+          opacity: 0;
+          pointer-events: none;
+        }
+      `}</style>
     </div>
   );
 }
